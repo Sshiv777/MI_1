8acfbe5b52737b70dcd925eed9106c95b2aa054b
Shiv <rajputshiv777@gmail.com>
2026-06-26 03:10:45 +0530

    add auth middleware and login logout controller

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -49,18 +49,17 @@ const userSchema = new mongoose.Schema({
 userSchema.pre("save" , async function(next){
 
     if(!this.isModified("password")){
-        return next;
+        return 
     }
 
     this.password = await bcrypt.hash(this.password , 10)
-    next
 })
 
 userSchema.methods.isPasswordCorrect = async function(password){
     return await bcrypt.compare( password , this.password)
 }
 
-userSchema.methods.generateAccessToken = async function(){
+userSchema.methods.generateAccessToken =  function(){
     return jwt.sign(
         {
             _id: this._id,
@@ -75,7 +74,7 @@ userSchema.methods.generateAccessToken = async function(){
     )
 }
 
-userSchema.methods.generateRefreshToken = async function(){
+userSchema.methods.generateRefreshToken =  function(){
     return jwt.sign(
         {
             _id: this._id,
