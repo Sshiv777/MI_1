0500f4f1d7b7f7f04a0ea1da0e87cf9bb5fb5736
Shiv <rajputshiv777@gmail.com>
2026-06-25 18:00:26 +0530

    all register controller bug fix

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -17,7 +17,6 @@ const registerUser = asyncHandler( async (req , res) => {
     // return response
 
     const { fullname , email , username , password } = req.body
-    console.log("email: " , email);
 
     if(
         [fullname , email , username , password].some((field)=>
@@ -27,7 +26,7 @@ const registerUser = asyncHandler( async (req , res) => {
         throw new ApiError(400 , "All fields are required")
     }
 
-    const existedUser = User.findOne({
+    const existedUser = await User.findOne({
         $or: [{username} ,{email}]
     })
 
@@ -35,8 +34,8 @@ const registerUser = asyncHandler( async (req , res) => {
         throw new ApiError(409 , "User with email or username already exists")
     }
 
-    const avatarLocalPath = req.files?.avatar[0]?.path;
-    const coverImageLocalPath = req.files?.coverImage[0]?.path; 
+    const avatarLocalPath = req.files?.avatar?.[0]?.path;
+    const coverImageLocalPath = req.files?.coverImage?.[0]?.path;
 
     if(!avatarLocalPath){
         throw new ApiError(400 , "Avatar file is required")
--- src/utils/cloudinary.js
+++ src/utils/cloudinary.js
@@ -3,9 +3,9 @@ import fs from 'fs'
 
 // Configuration
 cloudinary.config({ 
-    cloud_name: 'process.env.CLOUDINARY_CLOUD_NAME', 
-    api_key: 'process.env.CLOUDINARY_API_KEY', 
-    api_secret: 'process.env.CLOUDINARY_API_SECRET'
+    cloud_name: process.env.CLOUDINARY_CLOUD_NAME, 
+    api_key: process.env.CLOUDINARY_API_KEY, 
+    api_secret: process.env.CLOUDINARY_API_SECRET
 });
 
 const uploadOnCloudinary = async (localFilePath) => {
@@ -21,7 +21,9 @@ const uploadOnCloudinary = async (localFilePath) => {
         })
 
         // file has been uploaded successfully
-        console.log("file is uploaded on cloudinary" , response.url);
+        // console.log("file is uploaded on cloudinary" , response.url);
+
+        fs.unlinkSync(localFilePath)
 
         return response;
     }
